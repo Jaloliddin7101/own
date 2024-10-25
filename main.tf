c19139d89a157b4126e2d4a4a47ee5e32bb58713
Jaloliddin7101 <jleaderson@gmail.com>
2024-10-25 20:05:22 +0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.5"  
+    }
+  }
+  required_version = ">= 0.12"
+}
+
+provider "google" {
+  project = "natural-iridium-439511-m3"   
+  region  = "us-central1"            
+}
+
+
+
+resource "google_bigquery_dataset" "my_dataset" {
+  dataset_id = "my_dataset"  
+  location   = "US"           
+}
+
+resource "google_bigquery_table" "my_table" {
+  dataset_id = google_bigquery_dataset.my_dataset.dataset_id
+  table_id   = "my_table" 
+
+
+  schema = <<EOF
+[
+  {
+    "name": "id",             
+    "type": "STRING",         
+    "mode": "NULLABLE" 
+  }
+]
+EOF
+
+}
